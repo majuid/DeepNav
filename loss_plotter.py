db0c5dfed2457832e66834ac944c8b585c7fe305
majuid <62520510+majuid@users.noreply.github.com>
2020-12-01 01:27:44 +0200

    loss_plotter.py now takes the trials and zoom from command line

--- loss_plotter.py
+++ loss_plotter.py
@@ -10,13 +10,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import sys
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# trials you want to plot
-trials = [54, 71, 72]
+# trials you want to plot as a comma separated string (ex. 73,74,75)
+trials_str = sys.argv[1]
+trials = [int(trial) for trial in trials_str.split(',')]
+
 # if you want to plot starting from a certain epoch (for zooming purposes)
-zoom = 1
+zoom = int(sys.argv[2])
 
 legends = []
 train_loss_plots = plt.subplot(211)
@@ -42,15 +45,15 @@ for trial in trials:
     val_loss = df.val_loss
     
     # create epochs vector (x-axis)
-    epochs = np.linspace(zoom, len(train_loss)-1, len(train_loss))
+    epochs = list(range(zoom, len(train_loss)))
     
     # print total epochs, minimum loss and val_loss along with their epochs
     print("trial:", trial, ", epochs:", val_loss.size, end=", ")
     print("min training loss:", f'{np.min(train_loss):.4f}', "at epoch", np.argmin(train_loss), end=", ") 
     print("min validation loss:", f'{np.min(val_loss):.4f}', "at epoch", np.argmin(val_loss),)
 
-    train_loss_plots.plot(epochs[zoom:], train_loss[zoom:])
-    val_loss_plots.plot(epochs[zoom:], val_loss[zoom:])
+    train_loss_plots.plot(epochs, train_loss[zoom:])
+    val_loss_plots.plot(epochs, val_loss[zoom:])
     
 
 train_loss_plots.grid(True)
