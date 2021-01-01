66b2128db61f5e238430a63351f72cbf426fce66
majuid <62520510+majuid@users.noreply.github.com>
2021-01-01 10:58:58 +0200

    save the model in SavedModel format and convert it to tf lite

--- DeepNav.py
+++ DeepNav.py
@@ -94,10 +94,23 @@ for trial_offset, hyperparam_value in enumerate(hyperparam_values):
     # start training
     model = training.start_training(session_data, model_architecture, train_dataset, val_dataset, \
                                     signals_weights_tensor, trial_tree)
-
+    
     # for every flight, plot all states (truth vs predictions)
     flights_summary = postprocessing.evaluate_all_flights(model, train_flights_dict, val_flights_dict, \
-                                        trial_tree["trial_root_folder"], n_extreme_flights=30)
+                                        trial_tree["trial_root_folder"], n_extreme_flights=10)
 
     # add the network configuration and performance to the summary csv
     postprocessing.summarize_session(trial_tree, model, session_data, flights_summary)
+
+    # save the model in tf SavedModel format
+    model_path = trial_tree["trial_root_folder"] + "/saved_model"
+    tf.saved_model.save(model, model_path)
+
+    # Convert the model into lite format (to use with embedded systems)
+    converter = tf.lite.TFLiteConverter.from_saved_model(model_path)
+    tflite_model = converter.convert()
+
+    # Save the lite model
+    with open(trial_tree["trial_root_folder"] + '/model.tflite', 'wb') as f:
+        f.write(tflite_model)
+
